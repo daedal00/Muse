aa8f5e13fb2405e17e4de10a9e755e4da80318e0
daedal00 <samuelkim019@gmail.com>
2025-06-25 16:41:07 -0700

    ignore generated in test

--- backend/graph/schema.resolvers.go
+++ backend/graph/schema.resolvers.go
@@ -707,22 +707,3 @@ func (r *Resolver) Subscription() SubscriptionResolver { return &subscriptionRes
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type subscriptionResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func encodeCursor(id string) string {
-	return base64.StdEncoding.EncodeToString([]byte(id))
-}
-func decodeCursor(cursor string) (string, error) {
-	data, err := base64.StdEncoding.DecodeString(cursor)
-	if err != nil {
-		return "", fmt.Errorf("invalid cursor")
-	}
-	return string(data), nil
-}
-*/
